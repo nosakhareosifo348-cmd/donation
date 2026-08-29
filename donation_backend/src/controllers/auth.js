7f99206b572d6d42e6667a92bb90bb8191c18aef
Mandela-Inegbedion <inegbedionmandela9@gmail.com>
2026-08-29 16:56:38 +0100

    Remove JWT expiry

--- donation_backend/src/controllers/auth.js
+++ donation_backend/src/controllers/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const Admin = require('../models/Admin')
 
 function signToken(id) {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN || '7d' })
+  return jwt.sign({ id }, process.env.JWT_SECRET)
 }
 
 // POST /api/auth/login
